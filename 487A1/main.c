55050d8546814ffb429dbda7ccc89894a4552232
GamePC\hutch <hutchinson9736@gmail.com>
2024-01-20 15:41:48 -0800

    added header and command line arguments - still need to create makefie

--- 487A1/main.c
+++ 487A1/main.c
@@ -1,17 +1,50 @@
+#include "complex.h"
 #include "stdio.h"
+#include "string.h"
+#include "stdbool.h"
 
 #define STRING_MAX_SIZE 128
 
 char inputdata[STRING_MAX_SIZE] = "";
 
-struct Complex {
-	float real, imaginary;
-};
+int main(int numArguments, char* arguments[]) {
 
-int main(void) {
+	bool invalid = false;
+
+	if (numArguments == 2) {
+		if (strcmp(arguments[1], "-i") == 0) {
+			// interactive mode function
+		}
+		if (strcmp(arguments[1], "-b") == 0) {
+			// batch mode function
+		}
+		else {
+			invalid = true;
+		}
+	}
+	else {
+		invalid = true;
+	}
+
+	if (invalid) {
+		fprintf(stderr, "Unknown or missing arguments.\n\n");
+		fprintf(stderr, "-i\t\tInteractive mode (user input/output).\n");
+		fprintf(stderr, "-b\t\tBatch mode(inputdata.txt input/output outputdata.txt).\n");
+		fprintf(stderr, "Enter only 4 numbers (Real + Imag).\n\n");
+	}
+
+
+
+
+
+
+
+
+
+
 	struct Complex num1, num2;
 
-	printf("Enter two imaginary numbers: ");
+	printf("Enter two imaginary numbers (Real + Imag): ");
 
 	if (scanf_s("%f %f %f %f", &num1.real, &num1.imaginary, &num2.real, &num2.imaginary) != 4) {
 		fprintf(stderr, "Enter only 4 numbers.\n");
