1c069b0e4a7c717c787be403a085eee25d94cbcc
GamePC\hutch <hutchinson9736@gmail.com>
2024-01-21 00:23:05 -0800

    ComplexAdd function kinda working?

--- 487A1/complex.c
+++ 487A1/complex.c
@@ -15,7 +15,7 @@ void InteractiveMode(void) {
 	}
 }
 void BatchMode(void) {
-	struct Complex num1, num2;
+	struct Complex num1, num2, result;
 
 	num1.real = num1.imaginary = 0;
 	num2.real = num2.imaginary = 0;
@@ -30,49 +30,55 @@ void BatchMode(void) {
 	results = fopen("results.txt", "w");
 
 	while (!feof(imaginarydata)){
-		//if (feof(imaginarydata)) {
-		//	break;
-		//}
-
 		fscanf(imaginarydata, "%c %f %f %f %f ", &operation, &num1.real, &num1.imaginary, &num2.real, &num2.imaginary);
 
 		//char inputbuffer[100] = { 0 };
 		//fread(inputbuffer, sizeof(char), sizeof(inputbuffer), imaginarydata);
 		//sscanf_s(inputbuffer, "%c %f %f %f %f ", &operation, &num1.real, &num1.imaginary, &num2.real, &num2.imaginary);
 
-		//switch (operation)
-		//{
-		//case 'A':
-		//case 'a':
-		//	printf("adding\n");
-		//	break;
-		//case 'S':
-		//case 's':
-		//	printf("subtracting\n");
-		//	break;
-		//case 'M':
-		//case 'm':
-		//	printf("multiplying\n");
-		//	break;
-		//case 'D':
-		//case 'd':
-		//	printf("dividing\n");
-		//	break;
-		//case 'Q':
-		//case 'q':
-		//	printf("Quitting....");
-		//	break;
-		//default:
-		//	printf("Quitting....");
-		//	break;
-		//}
+		switch (operation)
+		{
+		case 'A':
+		case 'a':
+			result = ComplexAdd(num1, num2);
+			
+			printf("adding\n");
+			break;
+		case 'S':
+		case 's':
+			printf("subtracting\n");
+			break;
+		case 'M':
+		case 'm':
+			printf("multiplying\n");
+			break;
+		case 'D':
+		case 'd':
+			printf("dividing\n");
+			break;
+		case 'Q':
+		case 'q':
+			printf("Quitting....");
+			break;
+		default:
+			printf("Quitting....");
+			break;
+		}
 
 		
-		fprintf(results, "%c %.3g j%.3g %.3g j%.3g \n", operation, num1.real, num1.imaginary, num2.real, num2.imaginary);
+		fprintf(results, "%.3g j%.3g \n", result.real, result.imaginary);
+		//fprintf(results, "%c %.3g j%.3g %.3g j%.3g \n", operation, num1.real, num1.imaginary, num2.real, num2.imaginary);
 		//fprintf(results, "%.3g j%.3g %.3g j%.3g\n", 1.34, -52.0, 134,1);
 	}
 
 	fclose(imaginarydata);
 	fclose(results);
 
+}
+
+struct Complex ComplexAdd(struct Complex num1, struct Complex num2) {
+	struct Complex result;
+	result.real = num1.real + num2.real;
+	result.imaginary = num2.imaginary + num2.imaginary;
+	return result;
 }
--- 487A1/complex.h
+++ 487A1/complex.h
@@ -1,6 +1,9 @@
 void InteractiveMode(void);
 void BatchMode(void);
 
+
 struct Complex {
 	float real, imaginary;
 };
+
+struct Complex ComplexAdd(struct Complex, struct Complex);
