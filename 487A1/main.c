84c819602ddb07a8620ffb883ea7b148c164043c
GamePC\hutch <hutchinson9736@gmail.com>
2024-01-22 13:07:15 -0800

    jk actually removed batch mode and unnecessary PrintResultFile function

--- 487A1/main.c
+++ 487A1/main.c
@@ -1,12 +1,5 @@
 #include "complex.h"
 #include "stdio.h"
-#include "string.h"
-#include "stdbool.h"
-
-
-#define STRING_MAX_SIZE 128
-
-char inputdata[STRING_MAX_SIZE] = "";
 
 int main(int numArguments, char* arguments[]) {
 
