6e80ee6371aa1dc3b7ba8a7d7ceb22da399a55bd
GamePC\hutch <hutchinson9736@gmail.com>
2024-01-22 13:13:38 -0800

    more clean up

--- 487A1/complex.c
+++ 487A1/complex.c
@@ -2,58 +2,6 @@
 #include "stdio.h"
 #include "io.h"
 
-void InteractiveMode(void) {
-	struct Complex num1, num2, result;
-
-	char operation = NULL;
-	char inputbuffer[100];
-	if (_isatty(_fileno(stdin))) {
-		fprintf(stderr, "Complex calculator\n\n");
-		fprintf(stderr, "Type a letter to specify the arithmetic operator (A, S, M, D)\n");
-		fprintf(stderr, "followed by two complex numbers expressed as pairs of doubles.\n");
-		fprintf(stderr, "Type Q to quit.\n\n");
-	}
-	while ((operation != 'Q') && (operation != 'q')) {
-		if (_isatty(_fileno(stdin))) {
-			fprintf(stderr, "Enter exp: ");
-		}
-		
-		fgets(inputbuffer, sizeof(inputbuffer), stdin);
-		sscanf(inputbuffer, "%c %f %f %f %f", &operation, &num1.real, &num1.imaginary, &num2.real, &num2.imaginary);
-
-		switch (operation)
-		{
-		case 'A':
-		case 'a':
-			result = ComplexAdd(num1, num2);
-			PrintResult(result);
-			break;
-		case 'S':
-		case 's':
-			result = ComplexSubtract(num1, num2);
-			PrintResult(result);
-			break;
-		case 'M':
-		case 'm':
-			result = ComplexMultiply(num1, num2);
-			PrintResult(result);
-			break;
-		case 'D':
-		case 'd':
-			result = ComplexDivide(num1, num2);
-			PrintResult(result);
-			break;
-		case 'Q':
-		case 'q':
-			//printf("Quitting....\n");
-			break;
-		default:
-			printf("Invalid operation.\n");
-			break;
-		}
-	}
-}
-
 struct Complex ComplexAdd(struct Complex num1, struct Complex num2) {
 	num1.real += num2.real;
 	num1.imaginary += num2.imaginary;
@@ -88,7 +36,7 @@ struct Complex ComplexDivide(struct Complex num1, struct Complex num2) {
 	return result;
 }
 
-void PrintResult(struct Complex result) {
+void PrintComplexNumber(struct Complex result) {
 	if (result.imaginary < 0) {
 		result.imaginary *= -1;
 		printf("%.6g - j %.6g \n", result.real, result.imaginary);
--- 487A1/main.c
+++ 487A1/main.c
@@ -1,9 +1,70 @@
 #include "complex.h"
 #include "stdio.h"
+#include "io.h"
+
+int isStreamTerminal(void);
 
 int main(int numArguments, char* arguments[]) {
 
-	InteractiveMode();
+	struct Complex num1, num2, result;
+
+	char operation = NULL;
+	char inputbuffer[100];
+
+	if (isStreamTerminal()) {
+		fprintf(stderr, "Complex calculator\n\n");
+		fprintf(stderr, "Type a letter to specify the arithmetic operator (A, S, M, D)\n");
+		fprintf(stderr, "followed by two complex numbers expressed as pairs of doubles.\n");
+		fprintf(stderr, "Type Q to quit.\n\n");
+	}
+
+	while ((operation != 'Q') && (operation != 'q')) {
+		if (isStreamTerminal()) {
+			fprintf(stderr, "Enter exp: ");
+		}
+
+		fgets(inputbuffer, sizeof(inputbuffer), stdin);
+		sscanf(inputbuffer, "%c %f %f %f %f", &operation, &num1.real, &num1.imaginary, &num2.real, &num2.imaginary);
+
+		switch (operation)
+		{
+		case 'A':
+		case 'a':
+			result = ComplexAdd(num1, num2);
+			PrintComplexNumber(result);
+			break;
+		case 'S':
+		case 's':
+			result = ComplexSubtract(num1, num2);
+			PrintComplexNumber(result);
+			break;
+		case 'M':
+		case 'm':
+			result = ComplexMultiply(num1, num2);
+			PrintComplexNumber(result);
+			break;
+		case 'D':
+		case 'd':
+			result = ComplexDivide(num1, num2);
+			PrintComplexNumber(result);
+			break;
+		case 'Q':
+		case 'q':
+			break;
+		default:
+			printf("Invalid operation.\n");
+			break;
+		}
+	}
 
 	return 0;
+}
+
+int isStreamTerminal(void) {
+	if (_isatty(_fileno(stdin))) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
 }
