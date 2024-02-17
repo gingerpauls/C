0fc5db4a4684a6c980ff9d603f244413be09f104
Paul Hutchinson <hutchinson9736@gmail.com>
2024-02-16 22:10:13 -0800

    had to add 1 char for null character

--- dynamicmemory/main.c
+++ dynamicmemory/main.c
@@ -1,10 +1,13 @@
 #define _CRT_SECURE_NO_WARNINGS
 
-#include "stdio.h"
-#include "stdlib.h"
-#include "string.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
 
 #define STRING_SIZE 100
+
 
 typedef struct
 {
@@ -14,7 +17,7 @@ typedef struct
 
 void PrintAccount ( Account *account_list, int num_accounts )
 {
-    printf ( "\nPrinting Acounts \n\n" );
+    printf ( "\nPrinting Accounts\n\n" );
     for ( size_t i = 0; i < num_accounts; i++ )
     {
         printf ( "account_list[%d]->email: %s\n", i, account_list[i].email );
@@ -28,26 +31,27 @@ int main ( void )
     int num_accounts = 0;
     Account *account_list = NULL;
 
-    printf ( "enter num accounts: " );
+    printf ( "Enter number of accounts: " );
     scanf ( "%d", &num_accounts );
     account_list = malloc ( sizeof ( *account_list ) * num_accounts );
 
     for ( size_t i = 0; i < num_accounts; i++ )
     {
-        printf ( "enter acct[%d] email: ", i );
+        printf ( "Enter acct[%d] email: ", i );
         scanf ( "%s", input_buffer );
-        account_list[i].email = malloc ( sizeof ( *account_list->email ) * strlen ( input_buffer ) );
+        account_list[i].email = malloc ( sizeof ( *account_list->email ) * ( strlen ( input_buffer ) + 1 ) );
         strcpy ( account_list[i].email, input_buffer );
-        printf ( "enter acct[%d] name: ", i );
+
+        printf ( "Enter acct[%d] name: ", i );
         scanf ( "%s", input_buffer );
-        account_list[i].name = malloc ( sizeof ( *account_list->name ) * strlen ( input_buffer ) );
+        account_list[i].name = malloc ( sizeof ( *account_list->name) * ( strlen ( input_buffer ) + 1 ) );
         strcpy ( account_list[i].name, input_buffer );
     }
     PrintAccount ( account_list, num_accounts );
     for ( size_t i = 0; i < num_accounts; i++ )
     {
-        free ( account_list[i].name );
         free ( account_list[i].email );
+        free ( account_list[i].name );
     }
     free ( account_list );
     return 0;
