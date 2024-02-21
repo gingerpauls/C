69a49ebe47792ab11dce3b894f15403c046a3a46
Paul Hutchinson <hutchinson9736@gmail.com>
2024-02-21 11:22:37 -0800

    fixed git

--- library2/library.c
+++ library2/library.c
@@ -1,11 +1,3 @@
-/*
-    I want:
-        to be able to load account data from a file
-        add an account
-        delete an account
-*/
-
-//#define _CRT_SECURE_NO_WARNINGS
 #include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
@@ -18,7 +10,7 @@
 #define MAX_ISADMIN_LENGTH (1 + 1)
 
 enum ACCOUNT_PROPERTY {
-    ID, 
+    ID,
     email,
     name,
     pw,
@@ -46,13 +38,12 @@ typedef struct {
 } AccountList;
 
 void GetInputStdIn(Input *input) {
-    scanf_s("%*s %s", input->buffer, input->size);
-    //if(scanf_s("%*s %s", input->buffer, input->size) == 0) {
-    //    printf("Error: Input too long.\n\n");
-    //    exit(EXIT_FAILURE);
-    //}
+    if(scanf_s("%s", input->buffer, input->size) == 0) {
+        printf("Error: Input too long.\n\n");
+        exit(EXIT_FAILURE);
+    }
 }
-void GetInputFile(FILE* account_stream, Input *input) {
+void GetInputFile(FILE *account_stream, Input *input) {
     if(fscanf_s(account_stream, "%*s %s", input->buffer, input->size) == 0) {
         printf("Error: Input too long.\n\n");
         exit(EXIT_FAILURE);
@@ -64,7 +55,7 @@ void BoundaryCheck(AccountList *account_list) {
         exit(EXIT_FAILURE);
     }
 }
-void CopyString(AccountList account_list, Input *input, int i, int ACCOUNT_PROPERTY ) {
+void CopyString(AccountList account_list, Input *input, int i, int ACCOUNT_PROPERTY) {
     switch(ACCOUNT_PROPERTY) {
         case email:
             if(strcpy_s(account_list.account[i].email, (strlen(input->buffer) + 1), input->buffer)) {
@@ -96,19 +87,37 @@ void CopyString(AccountList account_list, Input *input, int i, int ACCOUNT_PROPE
     }
 }
 int main(void) {
+    FILE *account_stream = NULL;
+    errno_t err;
+    err = fopen_s(&account_stream, "accounts.txt", "r");
+    if(err == 0) {
+        printf("Database (accounts.txt) was opened.\n");
+    }
+    else {
+        printf("Database (accounts.txt) was not opened.\n");
+        return 1;
+    }
+    fseek(account_stream, 0, SEEK_END);
+    if(ftell(account_stream) == 0) {
+        printf("Database (accounts.txt) found, but no accounts created.\n");
+        fclose(account_stream);
+        return 1;
+    }
+    rewind(account_stream);
     AccountList account_list = {
         .account = NULL,
         .memory_size = 0,
-        .num_accounts = 100,
+        .num_accounts = 0,
         .num_admins = 0,
         .heap_ptr = NULL};
-    Input input = {.buffer = NULL, .size = STRING_SIZE};
-    //while(scanf_s("%s", input.buffer, input.size) == 1) {
-    //    if(strcmp(input.buffer, "Email") == 0) {
-    //        account_list.num_accounts++;
-    //    }
-    //}
-
+    Input input = {.buffer = NULL, .size = 255};
+    while(!feof(account_stream)) {
+        fscanf_s(account_stream, "%s", input.buffer, input.size);
+        if(strcmp(input.buffer, "Email") == 0) {
+            account_list.num_accounts++;
+        }
+    }
+    rewind(account_stream);
     account_list.memory_size =
         (account_list.num_accounts * sizeof(Account)) +
         (account_list.num_accounts * (MAX_EMAIL_LENGTH) * sizeof(char)) +
@@ -118,43 +127,49 @@ int main(void) {
     account_list.account = malloc(account_list.memory_size);
     (char *) account_list.heap_ptr = (char *) account_list.account + (account_list.num_accounts * sizeof(Account));
     for(size_t i = 0; i < account_list.num_accounts; i++) {
-        GetInputStdIn(&input);
+        GetInputFile(account_stream, &input);
         account_list.account[i].email = (char *) account_list.heap_ptr;
         (char *) account_list.heap_ptr += (strlen(input.buffer) + 1) * sizeof(char);
         BoundaryCheck(&account_list);
         CopyString(account_list, &input, i, email);
-        GetInputStdIn(&input);
+
+        GetInputFile(account_stream, &input);
         account_list.account[i].name = (char *) account_list.heap_ptr;
         (char *) account_list.heap_ptr += (strlen(input.buffer) + 1) * sizeof(char);
         BoundaryCheck(&account_list);
         CopyString(account_list, &input, i, name);
-        GetInputStdIn(&input);
+
+        GetInputFile(account_stream, &input);
         account_list.account[i].pw = (char *) account_list.heap_ptr;
         (char *) account_list.heap_ptr += (strlen(input.buffer) + 1) * sizeof(char);
         BoundaryCheck(&account_list);
         CopyString(account_list, &input, i, pw);
-        GetInputStdIn(&input);
+
+        GetInputFile(account_stream, &input);
         account_list.account[i].isAdmin = (char *) account_list.heap_ptr;
         (char *) account_list.heap_ptr += (strlen(input.buffer) + 1) * sizeof(char);
         BoundaryCheck(&account_list);
         CopyString(account_list, &input, i, isAdmin);
     }
-    for(size_t i = 0; i < account_list.num_accounts; i++) {
-       printf_s("\nID \t\t%d\n", i);
-       printf_s("Email \t\t%s\n", account_list.account[i].email);
-       printf_s("Name \t\t%s\n", account_list.account[i].name);
-       printf_s("Password \t%s\n", account_list.account[i].pw);
-       printf_s("isAdmin \t%d\n", atoi(account_list.account[i].isAdmin));
-    }
-    //for(size_t i = 0; i < account_list.num_accounts; i++) {
-    //    printf_s("\nID \t\t%d\n", i);
-    //    printf_s("Email \t\t%p\n", &account_list.account[i].email);
-    //    printf_s("Name \t\t%p\n", &account_list.account[i].name);
-    //    printf_s("Password \t%p\n", &account_list.account[i].pw);
-    //    printf_s("isAdmin \t%p", &account_list.account[i].isAdmin);
-    //}
-    //fclose(account_stream);
-    free(account_list.account);
 
+    //for(size_t i = 0; i < account_list.num_accounts; i++) {
+    //   printf_s("\nID \t\t%d\n", i);
+    //   printf_s("Email \t\t%s\n", account_list.account[i].email);
+    //   printf_s("Name \t\t%s\n", account_list.account[i].name);
+    //   printf_s("Password \t%s\n", account_list.account[i].pw);
+    //   printf_s("isAdmin \t%d\n", atoi(account_list.account[i].isAdmin));
+    //}
+
+    for(size_t i = 0; i < account_list.num_accounts; i++) {
+        printf_s("\nID \t\t%d\n", i);
+        printf_s("Email \t\t%p\n", &account_list.account[i].email);
+        printf_s("Name \t\t%p\n", &account_list.account[i].name);
+        printf_s("Password \t%p\n", &account_list.account[i].pw);
+        printf_s("isAdmin \t%p", &account_list.account[i].isAdmin);
+    }
+
+
+    free(account_list.account);
+    fclose(account_stream);
     return 0;
 }
